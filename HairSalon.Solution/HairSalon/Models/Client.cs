072aa8ad6e296dcbf247fbaf499308ac6c476a5b
Eric Crudup <briefcasemanx@gmail.com>
2022-03-11 09:57:52 -0800

    adds properties to Client.cs

---
+++ HairSalon.Solution/HairSalon/Models/Client.cs
@@ -0,0 +1,34 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public string Name { get; set; }
+    public string HairColor { get; set;}
+    public int HairThickness { get; set; }
+    public int HairLength { get; set; }
+    public bool Curly { get; set; }
+    public int StylistId { get; set; }
+
+    public virtual Stylist Stylist { get; set; }
+  }
+}
+
+
+
+
+
+
+
+// namespace BestRestaurants.Models
+// {
+//   public class Restaurant
+//   {
+//     public string Name { get; set; }
+//     public int PricePoint { get; set; }
+//     public bool Vegan { get; set; }
+//     public int RestaurantId { get; set; }
+//     public int CuisineId { get; set; }
+
+//     public virtual Cuisine Cuisine { get; set; }
+//   }
+// }
