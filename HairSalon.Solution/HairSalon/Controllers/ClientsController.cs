6cb27f27703d5c4a38a2866c858c2e9e8251c679
Eric Crudup <briefcasemanx@gmail.com>
2022-03-14 08:49:02 -0700

    adds create() routes to ClientsController.cs

--- HairSalon.Solution/HairSalon/Controllers/ClientsController.cs
+++ HairSalon.Solution/HairSalon/Controllers/ClientsController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering; //For SelectList
+using Microsoft.EntityFrameworkCore;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +20,29 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      List<Client> model = _db.Clients.FirstOrDefault(cust => cust.ClientId == id);
+      List<Client> model = _db.Clients.ToList();
       return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client model = _db.Clients.FirstOrDefault(cust => cust.ClientId == id);
+      ViewBag.Stylist = _db.Stylists.FirstOrDefault(emp => emp.StylistId == model.StylistId);
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Specialty" );
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client cust)
+    {
+      _db.Clients.Add(cust);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
